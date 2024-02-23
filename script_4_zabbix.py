7161606ab1ce699d1cc4b64646c1fce1c98385a1
sergeMMikh <s.m.mih@mail.ru>
2024-02-23 12:03:33 +0000

    tested script for zabbix

--- script_4_zabbix.py
+++ script_4_zabbix.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+import re
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 9000))
-sock.sendall('Read'.encode())
-print(sock.recv(100))
-sock.close()
+def ask_server(sell_num: int) -> str:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('127.0.0.1', 9000))
+    sock.sendall(f'Get:{sell_num}'.encode())
+
+    message = sock.recv(10)
+
+    sock.close()
+
+    return message.decode()
+
+if __name__ == '__main__':
+    match sys.argv[1]:
+        case '-pv': 
+            print(float(ask_server(1)))
+        case '-sp': 
+            print(float(ask_server(2)))
+        case '-op': 
+            print(float(ask_server(4)))
+        case '-sprate': 
+            print(float(ask_server(35))/10)
+        case _:
+            print(f'unknown input: {sys.argv[1]}')
